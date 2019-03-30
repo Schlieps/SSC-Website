84ae73c206726bb0acfdc358d2ffe161843687c0
Derek <d.schlieps@gmail.com>
2019-03-30 09:51:37 +0000

    remove preloader, add critical css to header

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,3 @@
-// preloader
-    window.addEventListener('load', () => document.querySelector('.preloader').classList.add('hidePreloader'));
-
 $(document).ready(function(){
     
   //navbar toggler
